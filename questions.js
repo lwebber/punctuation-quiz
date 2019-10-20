1e1f01ddaed77213ba9a08c2049567ac7c9f9297
Laura Webber <wwebby1@gmail.com>
2019-10-20 15:07:48 -0700

    add questions.js

---
+++ questions.js
@@ -0,0 +1,26 @@
+const questions = [{
+        sent: "They hate their teacher.",
+        correct: "their",
+        options: ["their", "there", "they're", "theyre"]
+    },
+    {
+        sent: "It's fun.",
+        correct: "It's",
+        options: ["It's", "Its", "Itzz", "Itz"]
+    },
+    {
+        sent: "Theirs a bear over there.",
+        correct: "There's",
+        options: ["Theirs", "They'res", "There's", "Theres"]
+    },
+    {
+        sent: "It's fur is black.",
+        correct: "Its",
+        options: ["Itz", "Its", "Itzz", "It's"]
+    },
+    {
+        sent: "It's fun.",
+        correct: "It's",
+        options: ["It's", "Its", "Itzz", "Itz"]
+    },
+];
